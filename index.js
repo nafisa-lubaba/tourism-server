a1b4b0822a97cba7b0f0cd2356e42b3612956fcf
Acer <nafisalubaba71@gmail.com>
2024-04-28 17:48:21 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-// MfzVkIcROUiUsYeZ
-// tourism-site
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
+
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrsyrpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+// console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,12 +27,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const cardCollection = client.db('cardDB').collection('card')
+
+    app.get('/card', async (req, res) => {
+      const cursor = cardCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/card', async(req, res)=>{
+      const newCard = req.body
+      console.log(newCard)
+      const result = await cardCollection.insertOne(newCard)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
